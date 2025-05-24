1ab7008b02dbfc9c24c7bd92163685eeb1f49a36
Ramasamy3488 <91617803+Ramasamy3488@users.noreply.github.com>
2025-05-24 22:21:52 +0530

    Delete src/main/java/com/example/spring_crud/config directory

--- src/main/java/com/example/spring_crud/config/WebConfig.java
+++
@@ -1,9 +0,0 @@
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:/images/");  //  This matches mounted volume
-    }
-}
