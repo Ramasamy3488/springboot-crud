1de6483dbeb982d9e6e545df18c6161307da889b
Ramasamy3488 <91617803+Ramasamy3488@users.noreply.github.com>
2025-05-24 22:11:14 +0530

    Create WebConfig.java

---
+++ src/main/java/com/example/spring_crud/config/WebConfig.java
@@ -0,0 +1,9 @@
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:/images/");  //  This matches mounted volume
+    }
+}
