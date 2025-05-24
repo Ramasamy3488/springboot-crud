9ff34a1267a2613a9d40ac9cb6b6572765bd60be
Ramasamy3488 <91617803+Ramasamy3488@users.noreply.github.com>
2025-05-24 21:55:26 +0530

    Create WebConfig.java

---
+++ src/main/java/com/example/spring_crud/WebConfig.java
@@ -0,0 +1,8 @@
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:/images/");
+    }
+}
